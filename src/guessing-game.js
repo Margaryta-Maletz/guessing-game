c71242795a4726313e75323d8ded354d9b8b0aaf
Margaryta-Maletz <rita.maletz@gmail.com>
2021-09-07 01:13:14 +0300

    feat: solve this task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,29 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+        this.step = 0;
+        this.number = 0;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.step = Math.round((this.max - this.min) / 2);
+        this.number = this.min + this.step;
+        return this.number
     }
 
     lower() {
+        this.max = this.number;
 
     }
 
     greater() {
-
+        this.min = this.number;
     }
 }
 
